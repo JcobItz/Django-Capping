360d4b4e6b3450ccbc62f4b00dbc1eebe21ae7cc
Jacob Itz <36682582+JcobItz@users.noreply.github.com>
2018-12-02 21:02:57 -0500

    more

--- CAPsite/survey/urls.py
+++ CAPsite/survey/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('<int:question_num>/', views.index, name='index'),
+    path('<int:survey_id>/<int:question_num>/', views.index, name='index'),
     path('', views.index, name='index'),
     path('submit/', views.submit, name='submit'),
     path('admin/charts/<int:q>/', views.charttest, name='charttest'),
     path('ajax/update_session/', views.update_session, name='update_session'),
+    
 ]
