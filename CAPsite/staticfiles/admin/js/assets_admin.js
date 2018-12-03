ca17ea23473b854551a894ad6d79bc1d2b0a4447
richiehowelll <38297136+richiehowelll@users.noreply.github.com>
2018-12-02 21:23:23 -0500

    assets_admin update

--- CAPsite/staticfiles/admin/js/assets_admin.js
+++ CAPsite/staticfiles/admin/js/assets_admin.js
@@ -16,12 +16,14 @@ if($("#id_is_multiple_choice").is(':checked'))
 console.log("testing is working");
 $('[name=_addanother]').hide();
 $('[name=_continue]').hide();
+$("p.deletelink-box").hide();
 $("td.djn-td.field-is_multiple_choice").append("Is the question multiple choice?");
 $("td.djn-td.field-allow_multiple").append("Allow multiple options to be selected?");
 $("td.delete.djn-delete-handler.djn-model-survey-question").append("Delete question?");
 $("td.djn-td.field-question_num").prepend("Question number:")
 $("td.djn-td.field-question_text").prepend("Question text:");
 $("td.djn-td.field-allow_other").append("Allow users to enter their own choice?")
+$("td.delete.djn-delete-handler.djn-model-survey-choice").append("Delete choice?");
 $("th.djn-th.question.required").hide();
 $("th.djn-th.is-the-question-multiple-choice").hide();
 $("th.djn-th.allow-multiple-options-to-be-selected").hide();
